f948d3d02b448b20f4d69c63786a885a5f946dd9
dev-jsmh <jhonatasam7@gmail.com>
2025-10-30 16:47:26 -0500

    feat: project initialization
    contains the initial files and folders for the api
    
    Authored: Jhonatan Martinez developer.jhonatan.martinez@gmail.com

---
+++ src/main/java/com/devjsmh/icea/content_manager_service/ContentManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.devjsmh.icea.content_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContentManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContentManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjsmh/icea/content_manager_service/ContentManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjsmh.icea.content_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContentManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
