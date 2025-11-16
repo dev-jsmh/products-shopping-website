e37f9f527ba0b7b3f60ee9f8fea88d34b1afffdf
dev-jsmh <jhonatasam7@gmail.com>
2025-11-15 19:31:21 -0500

    feat: create a dto for content-types
    This dto will be used for API responses and more
    
    Authored-by: Jhonatan Martinez <developer.jhonatan.martinez@gmail.com>

---
+++ src/main/java/com/devjsmh/icea/content_manager_service/contentType/dtos/ContentTypeDto.java
@@ -0,0 +1,53 @@
+package com.devjsmh.icea.content_manager_service.contentType.dtos;
+
+import com.fasterxml.jackson.databind.JsonNode;
+
+public class ContentTypeDto {
+
+    private Long id;
+    private String name;
+    private String slug;
+    private String description;
+    private JsonNode fields;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSlug() {
+        return slug;
+    }
+
+    public void setSlug(String slug) {
+        this.slug = slug;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public JsonNode getFields() {
+        return fields;
+    }
+
+    public void setFields(JsonNode fields) {
+        this.fields = fields;
+    }
+
+}
