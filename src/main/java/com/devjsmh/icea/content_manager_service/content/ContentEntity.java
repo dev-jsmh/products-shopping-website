f0a7329befc6d439e44eac6792527cba8b0d2f48
dev-jsmh <jhonatasam7@gmail.com>
2025-11-28 10:17:12 -0500

    feat: create content entity model
    This model represents'content entries'based in a specified'content type'fields schema

---
+++ src/main/java/com/devjsmh/icea/content_manager_service/content/ContentEntity.java
@@ -0,0 +1,118 @@
+package com.devjsmh.icea.content_manager_service.content;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import com.devjsmh.icea.content_manager_service.contentType.ContentTypeEntity;
+import com.devjsmh.icea.content_manager_service.contentType.JsonAttributeMapper;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+/**
+ * 
+ * This model represents 'content entries' based
+ * in a specified 'content type' fields schema
+ * 
+ * @author Jhonatan Samuel Martinez
+ */
+
+@Entity
+@Table(name = "contents")
+
+public class ContentEntity {
+
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne()
+    @JoinColumn(name = "content_type_id")
+    private ContentTypeEntity contentType;
+
+    @Column(columnDefinition = "varchar(255)")
+    private String status;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = true)
+    private LocalDateTime publishedAt;
+
+    // this property is a json object
+    @Convert(converter = JsonAttributeMapper.class)
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(columnDefinition = "json")
+    private JsonNode data;
+
+    public ContentEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public ContentTypeEntity getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(ContentTypeEntity contentType) {
+        this.contentType = contentType;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public LocalDateTime getPublishedAt() {
+        return publishedAt;
+    }
+
+    public void setPublishedAt(LocalDateTime publishedAt) {
+        this.publishedAt = publishedAt;
+    }
+
+    public JsonNode getData() {
+        return data;
+    }
+
+    public void setData(JsonNode data) {
+        this.data = data;
+    }
+
+}
