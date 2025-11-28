fa85940d27ff7525cf4adba8f255213b3927f78f
dev-jsmh <jhonatasam7@gmail.com>
2025-11-28 11:38:43 -0500

    feat: create controller to handle http request
    The controller belongs to the content management module
    
    Authored-by: Jhonatan Martinez

---
+++ src/main/java/com/devjsmh/icea/content_manager_service/content/ContentController.java
@@ -0,0 +1,68 @@
+package com.devjsmh.icea.content_manager_service.content;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devjsmh.icea.content_manager_service.content.dtos.ContentDetailedDto;
+import com.devjsmh.icea.content_manager_service.content.dtos.ContentDto;
+import com.devjsmh.icea.content_manager_service.content.mappers.IContentMapper;
+import com.devjsmh.icea.content_manager_service.content.mappers.IContentWithContentTypeSummaryMapper;
+
+/**
+ * 
+ * Handles all requests related to content entity and triggers the
+ * correct methods from the service layer to perform an action
+ * 
+ * @author Jhonatan Samuel Martinez
+ */
+@RestController
+@RequestMapping("/api")
+public class ContentController {
+
+    private final ContentService contentService;
+    private final IContentMapper contentMapper;
+    private final IContentWithContentTypeSummaryMapper contentWithContentTypeMapper;
+
+    // inject dependencies
+    public ContentController(
+            ContentService contentService,
+            IContentMapper contentMapper,
+            IContentWithContentTypeSummaryMapper contentWithContentTypeMapper) {
+        this.contentService = contentService;
+        this.contentMapper = contentMapper;
+        this.contentWithContentTypeMapper = contentWithContentTypeMapper;
+    }
+
+    @GetMapping("/v1/contents/{slug}")
+    public ResponseEntity<List<ContentDetailedDto>> getAllV1(@PathVariable("slug") String contentTypeSlug) {
+        List<ContentEntity> response = this.contentService.getAllV1(contentTypeSlug);
+        List<ContentDetailedDto> dtoList = this.contentWithContentTypeMapper.toDtoList(response);
+        return ResponseEntity.ok().body(dtoList);
+    }
+
+    /**
+     * Saved a new content entry of a type
+     * 
+     * @param slug    friendly url of the content type that need to be created
+     * @param request body with fields of the new content entry
+     * @return content entries of the specified type
+     */
+    @PostMapping("/v1/contents/{slug}")
+    public ResponseEntity<ContentDetailedDto> createV1(@PathVariable("slug") String contentTypeSlug,
+            @RequestBody ContentDto request) {
+        ContentEntity tempEntity = this.contentMapper.toEntity(request);
+        ContentEntity response = this.contentService.createV1(contentTypeSlug, tempEntity);
+        // use a different mapper to map the response to a dto with a summaries content
+        // type
+        ContentDetailedDto dto = this.contentWithContentTypeMapper.toDto(response);
+        return ResponseEntity.ok().body(dto);
+    }
+
+}
