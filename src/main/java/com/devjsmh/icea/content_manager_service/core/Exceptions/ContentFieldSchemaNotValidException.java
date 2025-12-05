a511de4ccdf7f08560d9eae862120129ddb0a44f
dev-jsmh <jhonatasam7@gmail.com>
2025-12-05 18:06:19 -0500

    feat: create exception for invalid content data field schema
    This exception is supposed to be threw when a content data
    is validated base in a content type and it does not meet
    the field schema from the provide content type

---
+++ src/main/java/com/devjsmh/icea/content_manager_service/core/Exceptions/ContentFieldSchemaNotValidException.java
@@ -0,0 +1,92 @@
+package com.devjsmh.icea.content_manager_service.core.Exceptions;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ContentFieldSchemaNotValidException extends RuntimeException {
+
+    private static final Logger logger = LoggerFactory.getLogger(ContentFieldSchemaNotValidException.class);
+    private Long contentId;
+    private String contentType;
+    private Object errors;
+
+    /**
+     * Main constructore to create exceptions when a content data does not meet
+     * the content type field schema
+     * 
+     * @param contentId   id of the content
+     * @param contentType type of the content
+     */
+    public ContentFieldSchemaNotValidException(Long contentId, String contentType) {
+        super(String.format("The content id: {} does not meet the field schema from the type: {}", contentId, contentType));
+        this.contentId = contentId;
+        this.contentType = contentType;
+        this.errors = null;
+
+        // logs the warning message to the application log
+        logger.warn("The content id: {} does not meet the field schema from the type: {}", contentId, contentType);
+    }
+
+    /**
+     * Creates exceptions with more details about what has caused it
+     * 
+     * @param contentId   id of the content
+     * @param contentType type of the content
+     * @param errors a lists or a hashmap object with the error messages or stack trace
+     */
+    public ContentFieldSchemaNotValidException(Long contentId, String contentType, Object errors) {
+        super(String.format("The content id: {} does not meet the field schema from the type: {}", contentId, contentType));
+        this.contentId = contentId;
+        this.contentType = contentType;
+        this.errors = errors;
+
+        // logs the warning message to the application log
+        logger.warn("The content id: {} does not meet the field schema from the type: {}", contentId, contentType);
+    }
+
+    /**
+     * Creates an exception with a general message
+     * it is for situations where a more general message is prefered
+     * 
+     * @param message general message
+     */
+     public ContentFieldSchemaNotValidException(String message) {
+        super(message);
+        this.contentId = null;
+        this.contentType = null;
+        this.errors = null;
+    }
+
+    public Object getDetails() {
+        return errors;
+    }
+
+    public void setDetails(Object errors) {
+        this.errors = errors;
+    }
+
+    public Long getContentId() {
+        return contentId;
+    }
+
+    public void setContentId(Long contentId) {
+        this.contentId = contentId;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+    public Object getErrors() {
+        return errors;
+    }
+
+    public void setErrors(Object errors) {
+        this.errors = errors;
+    }
+
+}
