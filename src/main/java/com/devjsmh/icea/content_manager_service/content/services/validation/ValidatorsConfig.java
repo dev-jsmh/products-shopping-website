e1af75b3916325b1c1c8d5dd89a8324e48fb11cf
dev-jsmh <jhonatasam7@gmail.com>
2025-12-22 16:45:55 -0500

    feat: add 'datetime' field validator

--- src/main/java/com/devjsmh/icea/content_manager_service/content/services/validation/ValidatorsConfig.java
+++ src/main/java/com/devjsmh/icea/content_manager_service/content/services/validation/ValidatorsConfig.java
@@ -2,6 +2,7 @@ package com.devjsmh.icea.content_manager_service.content.services.validation;
 
 import org.springframework.context.annotation.Configuration;
 
+import com.devjsmh.icea.content_manager_service.content.services.validation.types.DateTimeFieldValidator;
 import com.devjsmh.icea.content_manager_service.content.services.validation.types.MediaFieldValidator;
 import com.devjsmh.icea.content_manager_service.content.services.validation.types.RichTextFieldValidator;
 import com.devjsmh.icea.content_manager_service.content.services.validation.types.TextFieldValidator;
@@ -20,16 +21,19 @@ public class ValidatorsConfig {
     private final TextFieldValidator textFieldValidator;
     private final MediaFieldValidator mediaFieldValidator;
     private final RichTextFieldValidator richTextFieldValidator;
+    private final DateTimeFieldValidator dateTimeFieldValidator;
 
     public ValidatorsConfig(
             FieldValidatorRegistry registry,
             TextFieldValidator textFieldValidator,
             MediaFieldValidator mediaFieldValidator,
-            RichTextFieldValidator richTextFieldValidator) {
+            RichTextFieldValidator richTextFieldValidator,
+            DateTimeFieldValidator dateTimeFieldValidator) {
         this.registry = registry;
         this.textFieldValidator = textFieldValidator;
         this.mediaFieldValidator = mediaFieldValidator;
         this.richTextFieldValidator = richTextFieldValidator;
+        this.dateTimeFieldValidator = dateTimeFieldValidator;
     }
 
     @PostConstruct
@@ -37,6 +41,7 @@ public class ValidatorsConfig {
         registry.add("text", textFieldValidator);
         registry.add("media", mediaFieldValidator);
         registry.add("richtext", richTextFieldValidator);
+        registry.add("datetime", dateTimeFieldValidator);
     }
 
 }
