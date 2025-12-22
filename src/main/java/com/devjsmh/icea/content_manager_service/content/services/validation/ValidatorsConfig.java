f93b4d4f5518543c0633eff1a17871d21f2c3e9b
dev-jsmh <jhonatasam7@gmail.com>
2025-12-22 16:32:53 -0500

    feat: add media field validator
    These validator will validate that the 'media'
    content type field has all the attibutes or
    configuration needed

--- src/main/java/com/devjsmh/icea/content_manager_service/content/services/validation/ValidatorsConfig.java
+++ src/main/java/com/devjsmh/icea/content_manager_service/content/services/validation/ValidatorsConfig.java
@@ -2,6 +2,7 @@ package com.devjsmh.icea.content_manager_service.content.services.validation;
 
 import org.springframework.context.annotation.Configuration;
 
+import com.devjsmh.icea.content_manager_service.content.services.validation.types.MediaFieldValidator;
 import com.devjsmh.icea.content_manager_service.content.services.validation.types.TextFieldValidator;
 
 import jakarta.annotation.PostConstruct;
@@ -16,17 +17,21 @@ public class ValidatorsConfig {
 
     private final FieldValidatorRegistry registry;
     private final TextFieldValidator textFieldValidator;
+    private final MediaFieldValidator mediaFieldValidator;
 
     public ValidatorsConfig(
             FieldValidatorRegistry registry,
-            TextFieldValidator textFieldValidator) {
+            TextFieldValidator textFieldValidator,
+            MediaFieldValidator mediaFieldValidator) {
         this.registry = registry;
         this.textFieldValidator = textFieldValidator;
+        this.mediaFieldValidator = mediaFieldValidator;
     }
 
     @PostConstruct
     public void registerValidators() {
         registry.add("text", textFieldValidator);
+        registry.add("media", mediaFieldValidator);
     }
 
 }
