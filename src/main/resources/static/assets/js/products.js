366f9f69cd7b8c1662a49dff63bbfaa60a3db48a
dev-jsmh <jhonatasam7@gmail.com>
2026-09-02 17:35:04 -0500

    feat: create page for product details
    I have created the necessary CSS JS and HTML for this page to work
    and also added a route for it in the HomeController so when a
    user request for this page the server can retrive it for him

---
+++ src/main/resources/static/assets/js/products.js
@@ -0,0 +1,198 @@
+const products = [
+    {
+        id: 1,
+        title: "Arduboard Uno R4 WiFi (compatible)",
+        image_url: "https://naylampmechatronics.com/4962-home_default/arduino-uno-r4-wifi.jpg",
+        image_alt: "arduino-uno-r4-wifi",
+        price: "114,000",
+        sku: "001326",
+        images: [
+            {
+                url: "./../assets/images/arduino-r4-wifi-1.webp",
+                alt: "arduino-r4-wifi"
+            },
+            {
+                url: "./../assets/images/arduino-r4-wifi-2.webp",
+                alt: "arduino-r4-wifi"
+            },
+            {
+                url: "./../assets/images/arduino-r4-wifi-3.webp",
+                alt: "arduino-r4-wifi"
+            },
+            {
+                url: "./../assets/images/arduino-r4-wifi-4.webp",
+                alt: "arduino-r4-wifi"
+            }
+        ],
+        short_description: "Tódulo 4G LTE CAT1 ESP32 con GPS integrado Q334, ideal para IoT. Compatible con redes LTE-FDD y GSM, ofrece conectividad Wi-Fi y Bluetooth. Incluye soporte para. No hay información de llegada de productos disponible en este momento.",
+        complete_description: "",
+        stock: 20
+
+    },
+    {
+        id: 2,
+        title: "Bicicleta Roadmaster",
+        image_url: "./../assets/images/bicicleta-roadmaster-1.webp",
+        image_alt: "bicicleta-roadmaster",
+        price: "156.990",
+        sku: "0032449",
+        images: [
+            {
+                url: "./../assets/images/bicicleta-roadmaster-1.webp",
+                alt: "bicicleta-roadmaster"
+            },
+            {
+                url: "./../assets/images/bicicleta-roadmaster-2.webp",
+                alt: "bicicleta-roadmaster"
+            },
+            {
+                url: "./../assets/images/bicicleta-roadmaster-3.webp",
+                alt: "bicicleta-roadmaster"
+            },
+            {
+                url: "./../assets/images/bicicleta-roadmaster-4.webp",
+                alt: "bicicleta-roadmaster"
+            },
+        ],
+        short_description: "Tódulo 4G LTE CAT1 ESP32 con GPS integrado Q334, ideal para IoT. Compatible con redes LTE-FDD y GSM, ofrece conectividad Wi-Fi y Bluetooth. Incluye soporte para. No hay información de llegada de productos disponible en este momento.",
+        complete_description: "",
+        stock: 20
+    },
+    {
+        id: 3,
+        title: "Cables Jumper X 20",
+        image_url: "./../assets/images/cables-jumper-1.webp",
+        image_alt: "cables-jumper",
+        price: "25,000",
+        sku: "001036",
+        images: [
+            {
+                url: "./../assets/images/cables-jumper-1.webp",
+                alt: "cables-jumper"
+            },
+            {
+                url: "./../assets/images/cables-jumper-2.webp",
+                alt: "cables-jumper"
+            }
+        ],
+        short_description: "Tódulo 4G LTE CAT1 ESP32 con GPS integrado Q334, ideal para IoT. Compatible con redes LTE-FDD y GSM, ofrece conectividad Wi-Fi y Bluetooth. Incluye soporte para. No hay información de llegada de productos disponible en este momento.",
+        complete_description: "",
+        stock: 20
+
+    },
+    {
+        id: 4,
+        title: "Multimetro Digital",
+        image_url: "./../assets/images/multimetro-1.webp",
+        image_alt: "multimetro-digital",
+        price: "45,000",
+        sku: "0934838",
+        images: [
+            {
+                url: "./../assets/images/multimetro-1.webp",
+                alt: "multimetro-digital"
+            },
+            {
+                url: "./../assets/images/multimetro-2.webp",
+                alt: "multimetro-digital"
+            },
+            {
+                url: "./../assets/images/multimetro-3.webp",
+                alt: "multimetro-digital"
+            },
+            {
+                url: "./../assets/images/multimetro-4.webp",
+                alt: "multimetro-digital"
+            }
+        ],
+        short_description: "Tódulo 4G LTE CAT1 ESP32 con GPS integrado Q334, ideal para IoT. Compatible con redes LTE-FDD y GSM, ofrece conectividad Wi-Fi y Bluetooth. Incluye soporte para. No hay información de llegada de productos disponible en este momento.",
+        complete_description: "",
+        stock: 20
+    },
+    {
+        id: 5,
+        title: "Tarjeta RISC-V Sipeed Longan Nano - GD32VF103CBT6",
+        image_url: "https://naylampmechatronics.com/3149-home_default/tarjeta-risc-v-sipeed-longan-nano-gd32vf103cbt6.jpg",
+        image_alt: "tarjeta-risc-v-sipeed-longan-nano-gd32vf103cbt6",
+        price: "80,000",
+        sku: "000847",
+        images: [],
+        short_description: "",
+        complete_description: "",
+        stock: 20
+
+    },
+    {
+        id: 6,
+        title: "Alexa Parlante Inteligente Amazon Echo Dot Negro 5.ª Generación",
+        image_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSbZN-xX0YLsBywue3no7tPzwybgwqwZOFPa2tI5f3KZg&s=10",
+        image_alt: "Alexa Parlante Inteligente Amazon Echo Dot Negro 5.ª Generación",
+        price: "239,243",
+        sku: "000346",
+        images: [],
+        short_description: "",
+        complete_description: "",
+        stock: 20
+
+    },
+    {
+        id: 7,
+        title: "Raspberry Pi 5 - 2GB",
+        image_url: "https://naylampmechatronics.com/5557-home_default/raspberry-pi-5-2gb.jpg",
+        image_alt: "raspberry-pi-5-2gb",
+        price: "350,000",
+        sku: "0001479",
+        images: [],
+        short_description: "",
+        complete_description: "",
+        stock: 20
+    },
+    {
+        id: 8,
+        title: "Chevrolet Sail 1.4 Ltz",
+        image_url: "https://http2.mlstatic.com/D_NQ_NP_726297-MCO113205744621_062026-O.webp",
+        image_alt: "Chevrolet Sail 1.4 Ltz",
+        price: "35.990.000",
+        sku: "0004583",
+        images: [],
+        short_description: "",
+        complete_description: "",
+        stock: 20
+    },
+    {
+        id: 9,
+        title: "Raspberry pi 5 2gb",
+        image_url: "https://naylampmechatronics.com/5552-medium_default/raspberry-pi-5-2gb.jpg",
+        image_alt: "raspberry-pi-5-2gb",
+        price: "103,939",
+        sku: "9838339",
+        images: [],
+        short_description: "",
+        complete_description: "",
+        stock: 20
+    },
+    {
+        id: 10,
+        title: "Suzuki Jimny 1.4 Glx 5p",
+        image_url: "https://http2.mlstatic.com/D_NQ_NP_2X_935710-MCO111816213411_052026-F.webp",
+        image_alt: "Suzuki Jimny 1.4 Glx 5p",
+        price: "117.690.000",
+        sku: "0009467",
+        images: [],
+        short_description: "",
+        complete_description: "",
+        stock: 20
+    },
+    {
+        id: 11,
+        title: "Reloj Hombre Casio Digital Plateado A168wa-1wdf Plateado Negro",
+        image_url: "https://http2.mlstatic.com/D_NQ_NP_2X_664644-MLA99991837253_112025-F.webp",
+        image_alt: "Reloj Hombre Casio Digital Plateado A168wa-1wdf Plateado Negro",
+        price: "156.990",
+        sku: "0002543",
+        images: [],
+        short_description: "",
+        complete_description: "",
+        stock: 20
+    }
+];
