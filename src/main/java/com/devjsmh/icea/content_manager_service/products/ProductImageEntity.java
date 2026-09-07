09b04b73e21f03b732dd790b854e93c5c2841c34
dev-jsmh <jhonatasam7@gmail.com>
2026-09-07 05:36:15 -0500

    feat: add jakarta annotations to product-image entity
    I just added the necessary jakarta annotations so it serves
    as structure for its own database table

--- src/main/java/com/devjsmh/icea/content_manager_service/products/ProductImageEntity.java
+++ src/main/java/com/devjsmh/icea/content_manager_service/products/ProductImageEntity.java
@@ -1,7 +1,18 @@
 package com.devjsmh.icea.content_manager_service.products;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "product_images")
 public class ProductImageEntity {
 
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String url;
     private String altText;
 
