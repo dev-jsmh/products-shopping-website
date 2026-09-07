2b21aa637793f2bc53908627f46798e51896296c
dev-jsmh <jhonatasam7@gmail.com>
2026-09-07 09:12:33 -0500

    feat: include product's images when a product is searched by id
    The findById method was added explicitely to the repository interface
    and I have add to it an annotation that creates a JOIN with the
    product_images table so when a product record is requested by id
    number it loads inmediately all the images related to it

--- src/main/java/com/devjsmh/icea/content_manager_service/products/IProductRepository.java
+++ src/main/java/com/devjsmh/icea/content_manager_service/products/IProductRepository.java
@@ -1,9 +1,14 @@
 package com.devjsmh.icea.content_manager_service.products;
 
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IProductRepository extends JpaRepository<ProductEntity, Long> {
 
+    @EntityGraph(attributePaths = "images")
+    Optional<ProductEntity> findById(Long id);
 }
