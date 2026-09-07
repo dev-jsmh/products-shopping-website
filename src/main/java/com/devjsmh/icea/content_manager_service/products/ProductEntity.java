1d1402245d8e29747402a9c03b731d93ef431e48
dev-jsmh <jhonatasam7@gmail.com>
2026-09-07 05:46:27 -0500

    feat: add jakarta annotations to product entity class
    Using this annotations I convert the entity class to a database table

--- src/main/java/com/devjsmh/icea/content_manager_service/products/ProductEntity.java
+++ src/main/java/com/devjsmh/icea/content_manager_service/products/ProductEntity.java
@@ -2,8 +2,18 @@ package com.devjsmh.icea.content_manager_service.products;
 
 import java.util.List;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "products")
 public class ProductEntity {
 
+    @Id()
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Double price;
