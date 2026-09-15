e7cd999dc7568527ab5b77151707ca4b1ba2899d
dev-jsmh <jhonatasam7@gmail.com>
2026-09-15 00:58:06 -0500

    refactor: remove unused javascript from product details page
    This javascript was meant to get an "id" from the url,
    look for the product on an array, set it's stock availability,
    and render some data about the product. but this is no
    needed anymore since now the data is render on server-side

--- src/main/resources/static/assets/js/product-details.js
+++ src/main/resources/static/assets/js/product-details.js
@@ -8,15 +8,6 @@ let qtyInput = document.getElementById("quantity-wanted");
 
 const $ = window.document.querySelector.bind(document);
 
-let params = new URLSearchParams(window.location.search);
-const productId = params.get("id");
-
-const currentProduct = products.find((p) => {
-    if (p.id == productId) {
-        return p;
-    }
-});
-
 let imagesQuantity = 0;
 // queries DOM to access children nodes of thumbnail gallery
 const galleryThumbnail = $(".swiper.gallery-thumbnail > .swiper-wrapper");
@@ -26,56 +17,6 @@ if (galleryThumbnail != null && galleryThumbnail.childElementCount > 1) {
 } else {
     imagesQuantity = "auto";
 }
-
-function stockStatus(stock) {
-
-    let config = {
-        color: "",
-        status: ""
-    };
-
-    if (stock == 0) {
-        config.status = "No Disponible";
-        config.color = "#ff0000";
-        return;
-    }
-
-    if (stock > 0 && stock < 5) {
-        config.status = "Últimas Unidades";
-        config.color = "#ff9741ff";
-        return;
-    }
-
-    config.color = "#4cbb6c";
-    config.status = "En Stock";
-    return config;
-}
-
-let available = $("#product-available");
-
-available.style.color = stockStatus(currentProduct.stock).color;
-available.innerHTML = stockStatus(currentProduct.stock).status;
-
-/** Initialize product details */
-function renderProductDetails() {
-
-    let name = $("#product-name");
-    let sku = $("#product-sku");
-    let description = $("#product-description");
-    let price = $("#product-current-price");
-    let available = $("#product-available");
-
-    name.innerHTML = currentProduct.title;
-    sku.innerHTML = "#SKU " + currentProduct.sku;
-    description.querySelector("p").innerHTML = currentProduct.short_description;
-    price.innerHTML = "$ " + currentProduct.price;
-
-    available.style.color = stockStatus(currentProduct.stock).color;
-    available.innerHTML = stockStatus(currentProduct.stock).status;
-
-    setImageGallery(currentProduct);
-}
-
 
 addQuantityButton.onclick = () => {
     qtyInput.value++;
@@ -107,5 +48,3 @@ let productGallery = new Swiper(".product-gallery", {
         swiper: productThumbsnailGallery
     }
 });
-
-// window.document.addEventListener("DOMContentLoaded", renderProductDetails);
