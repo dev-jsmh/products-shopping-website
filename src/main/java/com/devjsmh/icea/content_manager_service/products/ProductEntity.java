6bb8c21b851ff36ea1773e010c927b46b455bb81
dev-jsmh <jhonatasam7@gmail.com>
2026-09-25 18:36:59 -0500

    feat: add TEXT column for product description

--- src/main/java/com/devjsmh/icea/content_manager_service/products/ProductEntity.java
+++ src/main/java/com/devjsmh/icea/content_manager_service/products/ProductEntity.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +24,9 @@ public class ProductEntity {
     private String sku;
     private String image_url;
     private String image_alt;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
 
     @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<ProductImageEntity> images = new ArrayList<>();
@@ -82,6 +86,14 @@ public class ProductEntity {
 
     public void setImage_alt(String image_alt) {
         this.image_alt = image_alt;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public List<ProductImageEntity> getImages() {
